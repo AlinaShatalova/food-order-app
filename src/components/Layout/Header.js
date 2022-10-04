fde4070b231f56ab53cbc5c1db4beac72abbd735
AlinaShatalova <lina.shatalova@yandex.ru>
2022-10-04 23:43:31 +0300

    add meals list

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -3,17 +3,17 @@ import mealsImage from '../../assets/meals.jpeg';
 import classes from './Header.module.css';
 
 const Header = () => {
-    return (
-        <>
-            <header className={classes.header}>
-                <h1>ReactMeals</h1>
-                <HeaderCartButton />
-            </header>
-            <div className={classes['main-image']}>
-                <img src={mealsImage} alt='A table with yummy food' />
-            </div>
-        </>
-    )
+  return (
+    <>
+      <header className={classes.header}>
+        <h1>ReactMeals</h1>
+        <HeaderCartButton />
+      </header>
+      <div className={classes['main-image']}>
+        <img src={mealsImage} alt='A table with yummy food' />
+      </div>
+    </>
+  )
 };
 
 export default Header;
